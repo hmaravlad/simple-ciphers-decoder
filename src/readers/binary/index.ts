98fd448b338f627304069bec5830779eba1b9373
Vlad Hmara <hmara.vladislav2@gmail.com>
2021-10-20 21:24:11 +0300

    remove debug console logs

--- src/readers/binary/index.ts
+++ src/readers/binary/index.ts
@@ -7,9 +7,7 @@ export class BinaryReader implements IReader {
     }
     const bytes: number[] = [];
     for (let i = 0; i < text.length; i += 8) {
-      console.dir({ text, part: text.slice(i, i + 8) });
       const byte = parseInt(text.slice(i, i + 8), 2);
-      console.dir({ byte, byte2: byte.toString(2) });
       bytes.push(byte);
     }
     return bytes;
