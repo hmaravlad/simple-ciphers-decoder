e09f3c1f8d04f344c2fda1665c4df07940d80ebb
Vlad Hmara <hmara.vladislav2@gmail.com>
2021-10-20 21:33:43 +0300

    add ascii reader

--- src/readers/ascii/index.ts
+++ src/readers/ascii/index.ts
@@ -1,8 +1,7 @@
 import { IReader } from 'src/types/reader';
 
 export class AsciiReader implements IReader {
-  // eslint-disable-next-line @typescript-eslint/no-unused-vars
   read(text: string): number[] {
-    throw new Error('NOT IMPLEMENTED');
+    return [...Buffer.from(text, 'ascii')];
   }
 }
