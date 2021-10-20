33733f1b3e2da5d0004087934d6c896da0b86e9a
Vlad Hmara <hmara.vladislav2@gmail.com>
2021-10-20 20:59:39 +0300

    add reader for binary

--- src/readers/binary/index.ts
+++ src/readers/binary/index.ts
@@ -1,8 +1,17 @@
 import { IReader } from 'src/types/reader';
 
 export class BinaryReader implements IReader {
-  // eslint-disable-next-line @typescript-eslint/no-unused-vars
-  read(text: string): Uint8Array {
-    throw new Error('NOT IMPLEMENTED');
+  read(text: string): number[] {
+    if (text.length < 8 || text.length % 8 !== 0) {
+      throw new Error('invalid binary text');
+    }
+    const bytes: number[] = [];
+    for (let i = 0; i < text.length; i += 8) {
+      console.dir({ text, part: text.slice(i, i + 8) });
+      const byte = parseInt(text.slice(i, i + 8), 2);
+      console.dir({ byte, byte2: byte.toString(2) });
+      bytes.push(byte);
+    }
+    return bytes;
   }
 }
