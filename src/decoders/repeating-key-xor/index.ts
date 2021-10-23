5e12158d9ba42962def32e40e58fcdfd99e2e869
hmaravlad <hmara.vladislav2@gmail.com>
2021-10-23 14:16:11 +0300

    add decoder for repeating-key-xor cipher

--- src/decoders/repeating-key-xor/index.ts
+++ src/decoders/repeating-key-xor/index.ts
@@ -1,10 +1,50 @@
 import { IDecoder } from 'src/types/decoder';
+import { Filter } from '../utils/filter';
+import { FrequencyAnalyzer } from './frequency-analyzer';
 
 const SHARPNESS_CRITERION = 0.3;
 
+type Str = number[];
+
+function textGroupShuffle(texts: Str[][], keyLength: number, step = 0): Str[][] {
+  if (step === keyLength) return [[]];
+  let result: Str[][] = [];
+  for (let i = 0; i < texts[step].length; i++) {
+    result = result.concat(textGroupShuffle(texts, keyLength, step + 1).map(arr => {
+      const arr1 = [...arr];
+      arr1.push(texts[step][i]);
+      return arr1;
+    }));
+  }
+  return result;
+}
+
 export class RepeatingKeyXorDecoder implements IDecoder {
   decrypt(ciphertext: number[]): string {
-    return this.getKeyLength(ciphertext).toString();
+    const keyLength = this.getKeyLength(ciphertext);
+    const frequencyAnalyzer = new FrequencyAnalyzer();
+    const ciphertexts: number[][] = new Array(keyLength).fill(0).map(() => []);
+    ciphertext.map((char, i) => {
+      ciphertexts[i % keyLength].push(char);
+    });
+    const textGroups = textGroupShuffle(ciphertexts.map(ctext => {
+      return frequencyAnalyzer.decrypt(ctext);
+    }), keyLength).map(arr => arr.reverse());
+    const allTexts = [];
+    for (let k = 0; k < textGroups.length; k++) {
+      const text: number[] = [];
+      for (let i = 0; i < textGroups[k][0].length; i++) {
+        for (let j = 0; j < keyLength; j++) {
+          if (textGroups[k][j][i] != undefined) {
+            text.push(textGroups[k][j][i]);
+          }
+        }
+      }
+      allTexts.push(text);
+    }
+    const filter = new Filter();
+    const allValidResults = filter.filterResults(allTexts.map(bytes => Buffer.from(bytes).toString('utf8')));
+    return allValidResults.join('\n\n======================================================\n\n\n');
   }
 
   getKeyLength(ciphertext: number[]): number {
